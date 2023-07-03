f8362a6301290a83e5db21585344bc4667792281
Chloe Darcy <chloe_darcy@icloud.com>
2023-07-03 00:51:12 -0400

    Setup for senting SMS

---
+++ project/message.py
@@ -0,0 +1,15 @@
+'''from twilio.rest import Client
+
+account_sid = "AC115c1585970f4ecafae7b8b9c440d22b"
+account_token = "e7570039470c2609ec6a28dd599cf67f"
+
+client = Client(account_sid, account_token)
+
+def sendMessage(text):
+    message = client.messages.create(
+        body=text,
+        from_='+18446781351',  # Your Twilio phone number
+        to='+16032757875'  # Your phone number
+    )
+'''
+# go to verified called IDs and add moms number
