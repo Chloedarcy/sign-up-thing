b26af2873fcd279f0eea86682dbc65747a48c6e1
Chloe Darcy <chloe_darcy@icloud.com>
2023-07-04 15:09:13 -0400

    working on messaging (it doesnt work)

--- project/message.py
+++ project/message.py
@@ -1,9 +1,13 @@
-'''from twilio.rest import Client
+from aiohttp import request
+from twilio.rest import Client
+from flask import Flask
+from twilio.twiml.messaging_response import MessagingResponse
 
 account_sid = "AC115c1585970f4ecafae7b8b9c440d22b"
-account_token = "e7570039470c2609ec6a28dd599cf67f"
+account_token = "e82fda8320c5b8a45a1070556f08a15c"
 
 client = Client(account_sid, account_token)
+
 
 def sendMessage(text):
     message = client.messages.create(
@@ -11,5 +15,5 @@ def sendMessage(text):
         from_='+18446781351',  # Your Twilio phone number
         to='+16032757875'  # Your phone number
     )
-'''
+
 # go to verified called IDs and add moms number
